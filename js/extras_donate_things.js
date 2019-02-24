3a6d003fba55be1f968874447abc449ec77519e1
Sriharsha Singam <harshasingam3@gmail.com>
2019-02-24 16:51:37 -0500

    added-gallery-images

--- js/extras_donate_things.js
+++ js/extras_donate_things.js
@@ -1,32 +1,32 @@
-var x = 1;
-function carouselMover() {
-    if (x === 1) {
-        console.log("gallery");
-        $(".parallax-slider").attr("src","images/Main1.jpeg");
-        x = 2;
-    } else if (x == 2){
-        $(".parallax-slider").attr("src","images/Carousel-01.jpg");
-        x = 3;
-    }else if (x == 3){
-        $(".parallax-slider").attr("src","images/Carousel-02.jpg");
-        x = 4;
-    }else if (x == 4){
-        $(".parallax-slider").attr("src","images/Carousel-03.jpg");
-        x = 5;
-    }else if (x == 5){
-        $(".parallax-slider").attr("src","images/Carousel-04.jpg");
-        x = 6;
-    }else if (x == 6){
-        $(".parallax-slider").attr("src","images/Carousel-05.jpg");
-        x = 0;
-    }else {
-        console.log("hero");
-        $(".parallax-slider").attr("src","images/hero-bg.jpg");
-        x = 1;
-    }
-}
+// var x = 1;
+// function carouselMover() {
+//     if (x === 1) {
+//         console.log("gallery");
+//         $(".parallax-slider").attr("src","images/Main1.jpeg");
+//         x = 2;
+//     } else if (x == 2){
+//         $(".parallax-slider").attr("src","images/Carousel-01.jpg");
+//         x = 3;
+//     }else if (x == 3){
+//         $(".parallax-slider").attr("src","images/Carousel-02.jpg");
+//         x = 4;
+//     }else if (x == 4){
+//         $(".parallax-slider").attr("src","images/Carousel-03.jpg");
+//         x = 5;
+//     }else if (x == 5){
+//         $(".parallax-slider").attr("src","images/Carousel-04.jpg");
+//         x = 6;
+//     }else if (x == 6){
+//         $(".parallax-slider").attr("src","images/Carousel-05.jpg");
+//         x = 0;
+//     }else {
+//         console.log("hero");
+//         $(".parallax-slider").attr("src","images/hero-bg.jpg");
+//         x = 1;
+//     }
+// }
 $(document).ready(function(){
-    setInterval(carouselMover,5000);
+    // setInterval(carouselMover,5000);
     // $("#submitSubscribe").on('click', function() {
     //     alert("This email: "+ $(".email").val() + "  is now Subscribed.");
     // });
@@ -88,13 +88,13 @@ $(document).ready(function(){
 
     });
 });
-
-$('.grid').masonry({
-    itemSelector: '.grid-item',
-    columnWidth: 160,
-    horizontalOrder: true
-});
-
-$("#contact_us").click(function (){
-    alert("Submit");
-});
+//
+// $('.grid').masonry({
+//     itemSelector: '.grid-item',
+//     columnWidth: 160,
+//     horizontalOrder: true
+// });
+//
+// $("#contact_us").click(function (){
+//     alert("Submit");
+// });
