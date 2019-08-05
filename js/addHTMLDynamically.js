57a8bea7b41b2cb5ae5e8caa122284d1fd8f293a
Sriharsha Singam <harshasingam3@gmail.com>
2019-08-05 11:01:31 -0400

    instagram-and-contacts-page

--- js/addHTMLDynamically.js
+++ js/addHTMLDynamically.js
@@ -86,6 +86,7 @@ function addHeader() {
     <div class="icon-bar">
       <a href="https://www.facebook.com/Food4Lives" class="facebook" target="_blank"><i class="fa fa-facebook"></i></a> 
       <a href="https://twitter.com/Food_For_Lives" class="twitter" target="_blank"><i class="fa fa-twitter"></i></a> 
+      <a href="https://www.instagram.com/food4livesatl" class="instagram" target="_blank"><i class="fa fa-instagram"></i></a> 
 <!--      <a href="#" class="google" target="_blank"><i class="fa fa-google"></i></a> -->
 <!--      <a href="#" class="linkedin" target="_blank"><i class="fa fa-linkedin"></i></a>-->
       <a href="https://www.youtube.com/channel/UCZ5EhUlijz4xwjaNUrtGXSw" class="youtube" target="_blank"><i class="fa fa-youtube"></i></a> 
@@ -136,3 +137,120 @@ function addHeader() {
 //         </div>
 //     </div>
 //
+
+
+function addContact() {
+    $("#contact-us-dynamic").replaceWith(`
+        <!-- contact
+    ================================================== -->
+    <section id="contact" class="s-contact">
+
+        <div class="overlay"></div>
+        <!--<div class="contact__line"></div>-->
+
+        <div class="row section-header" data-aos="fade-up">
+            <div class="col-full">
+                <h3 class="subhead">Contact Us</h3>
+                <h1 class="display-2 display-2--light">Please Reach out for next Event </h1>
+            </div>
+        </div>
+
+        <div class="row contact-content" data-aos="fade-up">
+
+            <div class="contact-primary">
+
+                <h3 class="h6">Send Us A Message</h3>
+                <form action="https://formspree.io/food4life.volunteers@gmail.com" method="POST">
+                    <fieldset>
+                        <div class="form-field" style="color: white;">
+                            <input type="text" name="name" id="contactName" placeholder="Your Name" value=""
+                                minlength="2" required="" aria-required="true" class="full-width" style="color: white;">
+                        </div>
+                        <div class="form-field">
+                            <input type="email" name="email" id="contactEmail" placeholder="Your Email" value=""
+                                required="" aria-required="true" class="full-width">
+                        </div>
+                        <div class="form-field">
+                            <textarea type="text" name="extra_mesage" id="contactMessage" placeholder="Your Message"
+                                rows="10" cols="50" required="" aria-required="true" class="full-width"></textarea>
+                        </div>
+                        <div class="form-field">
+                            <input type="submit" value="Submit" class="full-width btn--primary" id="contact_us">
+                            <div class="submit-loader">
+                                <div class="text-loader">Sending...</div>
+                                <div class="s-loader">
+                                    <div class="bounce1"></div>
+                                    <div class="bounce2"></div>
+                                    <div class="bounce3"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </fieldset>
+                </form>
+                <!-- contact-warning -->
+                <div class="message-warning">
+                    Something went wrong. Please try again.
+                </div>
+
+                <!-- contact-success -->
+                <div class="message-success">
+                    Your message was sent, thank you!<br>
+                </div>
+
+            </div>
+
+            <div class="contact-secondary">
+                <div class="contact-info">
+
+                    <h3 class="h6 hide-on-fullwidth">Contact Info</h3>
+
+                    <div class="cinfo">
+                        <h5>Where to Find Us</h5>
+                        <p>
+                            1122 Old Chattahoochee Ave NW, <br>
+                            Atlanta, GA <br>
+                            30318 US
+                        </p>
+                    </div>
+
+                    <div class="cinfo">
+                        <h5>Email Us At</h5>
+                        <p>
+                            contact@food4lives.org<br>
+
+                        </p>
+                    </div>
+
+                    <div class="cinfo">
+                        <h5>Call Us At</h5>
+                        <p>
+                            Phone: +1(404) 542-2572<br>
+                        </p>
+                    </div>
+
+                    <!-- <ul class="contact-social">
+                        <li>
+                            <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-behance" aria-hidden="true"></i></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-dribbble" aria-hidden="true"></i></a>
+                        </li>
+                    </ul>  end contact-social -->
+
+                </div> <!-- end contact-info -->
+            </div> <!-- end contact-secondary -->
+
+        </div> <!-- end contact-content -->
+
+    </section> <!-- end s-contact -->
+    `);
+}
