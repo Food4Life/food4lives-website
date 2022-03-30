6b5e1f70471271fa7cdfb49dc7d83a3e97e97bd4
Anish Visaria <anishvisaria98@gmail.com>
2022-03-30 12:12:16 -0400

    Update addHTMLDynamically.js

--- js/addHTMLDynamically.js
+++ js/addHTMLDynamically.js
@@ -213,7 +213,7 @@ function addContact() {
                     <div class="cinfo">
                         <h5>Call Us At</h5>
                         <p>
-                            Phone: +1 (404) 883-7039<br>
+                            Phone: +1 (770) 905-4727<br>
                         </p>
                     </div>
 
